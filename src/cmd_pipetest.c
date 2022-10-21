9a803b62278b0f31d0180a65101a79f0d377b215
Juha Manner <jumanner@student.hive.fi>
2022-10-21 15:23:38 +0300

    Added run_command() to pipeline.

--- src/cmd_pipetest.c
+++ src/cmd_pipetest.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 09:41:28 by jumanner          #+#    #+#             */
-/*   Updated: 2022/10/21 14:48:39 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/10/21 15:06:48 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,13 +31,28 @@ static void	connect_pipes(int read_pipe[2], int write_pipe[2])
 	}
 }
 
+static int	run_command(char *path, char *const *args, char *const *env, int read_pipe[2], int write_pipe[2])
+{
+	pid_t	pid;
+
+	if (read_pipe && write_pipe)
+		dup2(write_pipe[PIPE_READ], read_pipe[PIPE_WRITE]);
+	pid = fork();
+	if (pid == -1)
+		return (print_error(ERR_CHILD_PROC_FAIL, -1));
+	if (pid > 0)
+		return (0);
+	connect_pipes(read_pipe, write_pipe);
+	signal(SIGINT, SIG_DFL);
+	if (execve(path, args, env) == -1)
+		exit(print_error(ERR_CHILD_PROC_FAIL, 1));
+	return (1);
+}
+
 int	cmd_pipetest(char *const *args, t_state *state)
 {
-	pid_t	ls_pid;
 	char	*ls_args[4] = {"/bin/ls", "-l", "/", NULL};
-	pid_t	grep_pid;
 	char	*grep_args[3] = {"/usr/bin/grep", "A", NULL};
-	pid_t	wc_pid;
 	char	*wc_args[3] = {"/usr/bin/wc", "-l", NULL};
 
 	int		ls_to_grep[2] = {-1, -1}; // Read, write
@@ -51,40 +66,9 @@ int	cmd_pipetest(char *const *args, t_state *state)
 	if (pipe(grep_to_wc) == -1)
 		return (print_error("Failed to create pipe", 1));
 
-	ls_pid = fork();
-	if (ls_pid == -1)
-		return (print_error(ERR_CHILD_PROC_FAIL, 1));
-	if (ls_pid == 0)
-	{
-		connect_pipes(NULL, ls_to_grep);
-		signal(SIGINT, SIG_DFL);
-		if (execve(ls_args[0], ls_args, state->env) == -1)
-			exit(print_error(ERR_CHILD_PROC_FAIL, 1));
-	}
-
-	dup2(grep_to_wc[PIPE_READ], ls_to_grep[PIPE_WRITE]);
-
-	grep_pid = fork();
-	if (grep_pid == -1)
-		return (print_error(ERR_CHILD_PROC_FAIL, 1));
-	if (grep_pid == 0)
-	{
-		connect_pipes(ls_to_grep, grep_to_wc);
-		signal(SIGINT, SIG_DFL);
-		if (execve(grep_args[0], grep_args, state->env) == -1)
-			exit(print_error(ERR_CHILD_PROC_FAIL, 1));
-	}
-
-	wc_pid = fork();
-	if (wc_pid == -1)
-		return (print_error(ERR_CHILD_PROC_FAIL, 1));
-	if (wc_pid == 0)
-	{
-		connect_pipes(grep_to_wc, NULL);
-		signal(SIGINT, SIG_DFL);
-		if (execve(wc_args[0], wc_args, state->env) == -1)
-			exit(print_error(ERR_CHILD_PROC_FAIL, 1));
-	}
+	run_command(ls_args[0], ls_args, state->env, NULL, ls_to_grep);
+	run_command(grep_args[0], grep_args, state->env, ls_to_grep, grep_to_wc);
+	run_command(wc_args[0], wc_args, state->env, grep_to_wc, NULL);
 
 	wait(NULL);
 
