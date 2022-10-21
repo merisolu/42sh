e5f3ff645ac073b7c4b1a97c702287fa2c40f687
Juha Manner <jumanner@student.hive.fi>
2022-10-21 14:23:55 +0300

    Added grep to pipe test.

--- src/cmd_pipetest.c
+++ src/cmd_pipetest.c
@@ -6,35 +6,45 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 09:41:28 by jumanner          #+#    #+#             */
-/*   Updated: 2022/10/20 14:55:22 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/10/21 14:14:06 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+#include <stdio.h>
 
 int	cmd_pipetest(char *const *args, t_state *state)
 {
 	pid_t	ls_pid;
 	char	*ls_args[4] = {"/bin/ls", "-l", "/", NULL};
+	pid_t	grep_pid;
+	char	*grep_args[3] = {"/usr/bin/grep", "A", NULL};
 	pid_t	wc_pid;
 	char	*wc_args[3] = {"/usr/bin/wc", "-l", NULL};
-	int		pipe_file_des[2] = {-1, -1}; // Read, write
+
+	int		ls_to_grep[2] = {-1, -1}; // Read, write
+	int		grep_to_wc[2] = {-1, -1}; // Read, write
 
 	(void)args;
 
-	if (pipe(pipe_file_des) == -1)
-	{
-		ft_dprintf(STDERR_FILENO, "Error message");
-		return (1);
-	}
+	if (pipe(ls_to_grep) == -1)
+		return (print_error("Failed to create pipe", 1));
+
+	if (pipe(grep_to_wc) == -1)
+		return (print_error("Failed to create pipe", 1));
 
 	ls_pid = fork();
 	if (ls_pid == -1)
-		return (1);
+		return (print_error(ERR_CHILD_PROC_FAIL, 1));
 	if (ls_pid == 0)
 	{
-		close(pipe_file_des[PIPE_READ]);
-		dup2(pipe_file_des[PIPE_WRITE], STDOUT_FILENO);
+		if (dup2(ls_to_grep[PIPE_WRITE], STDOUT_FILENO) == -1)
+			perror("ls dup2");
+
+		close(ls_to_grep[PIPE_READ]);
+		close(ls_to_grep[PIPE_WRITE]);
+		close(grep_to_wc[PIPE_READ]);
+		close(grep_to_wc[PIPE_WRITE]);
 
 		signal(SIGINT, SIG_DFL);
 
@@ -42,13 +52,43 @@ int	cmd_pipetest(char *const *args, t_state *state)
 			exit(print_error(ERR_CHILD_PROC_FAIL, 1));
 	}
 
+	grep_pid = fork();
+	if (grep_pid == -1)
+		return (print_error(ERR_CHILD_PROC_FAIL, 1));
+	if (grep_pid == 0)
+	{
+		if (dup2(ls_to_grep[PIPE_READ], STDIN_FILENO) == -1)
+			perror("grep dup2 a");
+
+		close(ls_to_grep[PIPE_WRITE]);
+		close(ls_to_grep[PIPE_READ]);
+
+		if (dup2(grep_to_wc[PIPE_WRITE], STDOUT_FILENO) == -1)
+			perror("grep dup2 b");
+
+		close(grep_to_wc[PIPE_READ]);
+		close(grep_to_wc[PIPE_WRITE]);
+
+		signal(SIGINT, SIG_DFL);
+
+		if (execve(grep_args[0], grep_args, state->env) == -1)
+			exit(print_error(ERR_CHILD_PROC_FAIL, 1));
+	}
+
+	dup2(grep_to_wc[PIPE_READ], ls_to_grep[PIPE_WRITE]);
+
 	wc_pid = fork();
 	if (wc_pid == -1)
-		return (1);
+		return (print_error(ERR_CHILD_PROC_FAIL, 1));
 	if (wc_pid == 0)
 	{
-		close(pipe_file_des[PIPE_WRITE]);
-		dup2(pipe_file_des[PIPE_READ], STDIN_FILENO);
+		if (dup2(grep_to_wc[PIPE_READ], STDIN_FILENO) == -1)
+			perror("wc dup2");
+
+		close(grep_to_wc[PIPE_WRITE]);
+		close(grep_to_wc[PIPE_READ]);
+		close(ls_to_grep[PIPE_WRITE]);
+		close(ls_to_grep[PIPE_READ]);
 
 		signal(SIGINT, SIG_DFL);
 
@@ -58,8 +98,10 @@ int	cmd_pipetest(char *const *args, t_state *state)
 
 	wait(NULL);
 
-	close(pipe_file_des[0]);
-	close(pipe_file_des[1]);
+	close(ls_to_grep[0]);
+	close(ls_to_grep[1]);
+	close(grep_to_wc[0]);
+	close(grep_to_wc[1]);
 
 	return (0);
 }
