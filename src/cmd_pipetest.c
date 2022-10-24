9e7b0df7f3aafd1ae53a9505be5a94d628dff03b
Juha Manner <jumanner@student.hive.fi>
2022-10-24 12:57:45 +0300

    Added run_pipeline() to pipetest.

--- src/cmd_pipetest.c
+++ src/cmd_pipetest.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/20 09:41:28 by jumanner          #+#    #+#             */
-/*   Updated: 2022/10/21 15:06:48 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/10/24 12:56:53 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 static void	connect_pipes(int read_pipe[2], int write_pipe[2])
 {
-	if (read_pipe)
+	if (read_pipe[PIPE_READ] != -1)
 	{
 		if (dup2(read_pipe[PIPE_READ], STDIN_FILENO) == -1)
 			perror("left pipe");
 		close(read_pipe[PIPE_WRITE]);
 		close(read_pipe[PIPE_READ]);
 	}
-	if (write_pipe)
+	if (write_pipe[PIPE_READ] != -1)
 	{
 		if (dup2(write_pipe[PIPE_WRITE], STDOUT_FILENO) == -1)
 			perror("right pipe");
@@ -31,51 +31,70 @@ static void	connect_pipes(int read_pipe[2], int write_pipe[2])
 	}
 }
 
-static int	run_command(char *path, char *const *args, char *const *env, int read_pipe[2], int write_pipe[2])
+static pid_t	run_command(char *path, char *const *args, char *const *env, int read_pipe[2], int write_pipe[2])
 {
 	pid_t	pid;
 
-	if (read_pipe && write_pipe)
+	ft_dprintf(STDERR_FILENO, "Pipes: [%i, %i], [%i, %i]\n", read_pipe[0], read_pipe[1], write_pipe[0], write_pipe[1]);
+	if (read_pipe[0] != -1 && write_pipe[0] != -1)
 		dup2(write_pipe[PIPE_READ], read_pipe[PIPE_WRITE]);
 	pid = fork();
 	if (pid == -1)
 		return (print_error(ERR_CHILD_PROC_FAIL, -1));
 	if (pid > 0)
-		return (0);
+		return (pid);
 	connect_pipes(read_pipe, write_pipe);
 	signal(SIGINT, SIG_DFL);
 	if (execve(path, args, env) == -1)
 		exit(print_error(ERR_CHILD_PROC_FAIL, 1));
-	return (1);
+	return (0);
 }
 
+static pid_t	run_pipeline(char **commands, t_state *state)
+{
+	pid_t	result;
+	size_t	length;
+	int		read_pipe[2] = {-1, -1};
+	int		write_pipe[2] = {-1, -1};
+
+	length = ft_null_array_len((void *)commands);
+	for (size_t i = 0; i < length; i++)
+	{
+		if (i < length - 1)
+		{
+			if (pipe(write_pipe) == -1)
+				return (print_error("Failed to create pipe", 1));
+		}
+		else
+		{
+			write_pipe[PIPE_READ] = -1;
+			write_pipe[PIPE_WRITE] = -1;
+		}
+
+		result = run_command(ft_strsplit(commands[i], ' ')[0], ft_strsplit(commands[i], ' '), state->env, read_pipe, write_pipe);
+
+		close(read_pipe[PIPE_READ]);
+		close(read_pipe[PIPE_WRITE]);
+
+		read_pipe[PIPE_READ] = write_pipe[PIPE_READ];
+		read_pipe[PIPE_WRITE] = write_pipe[PIPE_WRITE];
+	}
+
+	close(read_pipe[PIPE_READ]);
+	close(read_pipe[PIPE_WRITE]);
+
+	return (result);
+}
+
+// pipetest "/bin/ls -l /|/usr/bin/grep A|/usr/bin/wc -l"
 int	cmd_pipetest(char *const *args, t_state *state)
 {
-	char	*ls_args[4] = {"/bin/ls", "-l", "/", NULL};
-	char	*grep_args[3] = {"/usr/bin/grep", "A", NULL};
-	char	*wc_args[3] = {"/usr/bin/wc", "-l", NULL};
+	char	**commands;
 
-	int		ls_to_grep[2] = {-1, -1}; // Read, write
-	int		grep_to_wc[2] = {-1, -1}; // Read, write
+	if (args[1] == NULL)
+		return (1);
 
-	(void)args;
-
-	if (pipe(ls_to_grep) == -1)
-		return (print_error("Failed to create pipe", 1));
-
-	if (pipe(grep_to_wc) == -1)
-		return (print_error("Failed to create pipe", 1));
-
-	run_command(ls_args[0], ls_args, state->env, NULL, ls_to_grep);
-	run_command(grep_args[0], grep_args, state->env, ls_to_grep, grep_to_wc);
-	run_command(wc_args[0], wc_args, state->env, grep_to_wc, NULL);
-
-	wait(NULL);
-
-	close(ls_to_grep[0]);
-	close(ls_to_grep[1]);
-	close(grep_to_wc[0]);
-	close(grep_to_wc[1]);
-
+	commands = ft_strsplit(args[1], '|');
+	waitpid(run_pipeline(commands, state), NULL, 0);
 	return (0);
 }
