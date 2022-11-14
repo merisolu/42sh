1d9dc2aad3f504cc73e4a5961ad22421811f7d59
Juha Manner <jumanner@student.hive.fi>
2022-11-14 11:27:14 +0200

    Fixed function parameter names in cursor.h.

--- include/cursor.h
+++ include/cursor.h
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/27 14:43:34 by jumanner          #+#    #+#             */
-/*   Updated: 2022/11/03 11:58:12 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/11/14 10:51:16 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,12 @@
 /* Files */
 
 /* cursor.c */
-void	save_cursor(t_input_context *state);
-void	load_cursor(t_input_context *state);
+void	save_cursor(t_input_context *context);
+void	load_cursor(t_input_context *context);
 
 /* cursor_utils.c */
-void	move_cursor_to_saved_position(t_input_context *state);
-size_t	cursor_get_column(t_input_context *state, size_t index);
-size_t	cursor_get_row(t_input_context *state, size_t index);
+void	move_cursor_to_saved_position(t_input_context *context);
+size_t	cursor_get_column(t_input_context *context, size_t index);
+size_t	cursor_get_row(t_input_context *v, size_t index);
 
 #endif
