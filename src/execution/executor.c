3495eddb611b4fe215041d28bfefb435d671df06
Juha Manner <jumanner@student.hive.fi>
2022-11-15 16:07:20 +0200

    Made executor.c norm compliant.

--- src/execution/executor.c
+++ src/execution/executor.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/31 13:39:02 by jumanner          #+#    #+#             */
-/*   Updated: 2022/11/15 15:46:18 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/11/15 16:06:29 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,29 +25,41 @@ static int	check_path_validity(char *path)
 	return (0);
 }
 
-pid_t	execute(char *const *args, t_state *state, t_ast_execution *ast)
+static int	find_path(char *const *args, t_state *state, char **path)
 {
-	char	*path;
 	int		return_value;
-	t_pipes	*pipes;
 
-	pipes = ast->pipes;
-	if (!args || !(args[0]) || !env_set("_", args[0], &(state->env)))
-		return (print_error(ERR_MALLOC_FAIL, -1));
-	if (built_in_get(args[0]))
-		return (built_in_run(built_in_get(args[0]), args, state, ast));
-	if (ft_strchr(args[0], '/') || (args[0][0] == '.'))
-	{
-		return_value = check_path_validity(args[0]);
-		if (return_value != 0)
-			return (return_value);
-		return (bin_execute(args[0], (char **)args, state->env, ast));
-	}
-	return_value = bin_env_find(args[0], state->env, &path);
+	return_value = bin_env_find(args[0], state->env, path);
 	if (return_value == 0)
 		return (print_named_error(
 				args[0], ERR_COM_NOT_FOUND, RETURN_COMMAND_NOT_FOUND));
 	else if (return_value == -1)
+		return (return_value);
+	return (1);
+}
+
+static int	execute_absolute_path(char *const *args, t_state *state, \
+t_ast_execution *ast)
+{
+	int	return_value;
+
+	return_value = check_path_validity(args[0]);
+	if (return_value != 0)
+		return (return_value);
+	return (bin_execute(args[0], (char **)args, state->env, ast));
+}
+
+pid_t	execute(char *const *args, t_state *state, t_ast_execution *ast)
+{
+	char	*path;
+	int		return_value;
+
+	if (built_in_get(args[0]))
+		return (built_in_run(built_in_get(args[0]), args, state, ast));
+	if (ft_strchr(args[0], '/') || (args[0][0] == '.'))
+		return (execute_absolute_path(args, state, ast));
+	return_value = find_path(args, state, &path);
+	if (return_value != 1)
 		return (return_value);
 	if (access(path, X_OK) == -1)
 		return (print_named_error(
