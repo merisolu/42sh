c4c67f1dbbb25ab549cf4edfc7c5c0954c52b859
Alexander Mann <amann@student.hive.fi>
2022-11-15 15:17:54 +0200

    added execute permissions check before running scripts

--- src/built-ins/cmd_execute_script.c
+++ src/built-ins/cmd_execute_script.c
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/14 15:28:55 by amann             #+#    #+#             */
-/*   Updated: 2022/11/14 16:40:09 by amann            ###   ########.fr       */
+/*   Updated: 2022/11/15 13:58:02 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,8 @@ int	cmd_execute_script(char *const *args, t_state *state)
 	char	*line;
 
 	fd = open(*args + 2, O_RDONLY);
-	if (fd == -1)
-		return (print_error(ERR_OPEN_FAIL, 1));
+	if (fd == -1 || access(*args + 2, X_OK))
+		return (print_error(ERR_NO_PERMISSION, 1));
 	ft_get_next_line(fd, &line);
 	if (!line)
 		return (1);
@@ -31,7 +31,8 @@ int	cmd_execute_script(char *const *args, t_state *state)
 			return (1);
 		ft_strclr(state->input_context.input);
 		ft_strcpy(state->input_context.input, line);
-		tokenize_and_execute(state);
+		if (ft_strlen(state->input_context.input) > 0)
+			tokenize_and_execute(state);
 		ft_strdel(&line);
 	}
 	state->running_script = FALSE;
