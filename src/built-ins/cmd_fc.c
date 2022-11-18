41a9b79d5169bc08571bef4f8fa121a0a04ed452
Juha Manner <jumanner@student.hive.fi>
2022-11-18 15:06:44 +0200

    Added support for s flag to built-in fc.

--- src/built-ins/cmd_fc.c
+++ src/built-ins/cmd_fc.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:03:39 by jumanner          #+#    #+#             */
-/*   Updated: 2022/11/18 14:37:45 by jumanner         ###   ########.fr       */
+/*   Updated: 2022/11/18 15:05:44 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,8 @@
 
 static void	on_error(char c)
 {
-	ft_dprintf(STDERR_FILENO, "fc: -%c: invalid option\nfc: usage: fc [-nl]\n",
-		c);
+	ft_dprintf(STDERR_FILENO, "21sh: fc: -%c: invalid option\n", c);
+	ft_putstr_fd("fc: usage: fc [-nl] or fc -s\n", STDERR_FILENO);
 }
 
 static int	get_start_index(t_state *state)
@@ -52,8 +52,15 @@ int	cmd_fc(char *const *args, t_state *state)
 	char	flags[6];
 	int		i;
 
-	(void)state;
-	i = parse_flags(args + 1, "elnrs", flags, &on_error) + 1;
+	i = parse_flags(args + 1, "lns", flags, &on_error) + 1;
+	if (ft_strchr(flags, 's'))
+	{
+		ft_putstr(state->history[1]);
+		ft_strclr(state->input_context.input);
+		ft_strcpy(state->input_context.input, state->history[1]);
+		tokenize_and_execute(state);
+		return (0);
+	}
 	if (ft_strchr(flags, 'l'))
 		print_history(!ft_strchr(flags, 'n'), state);
 	return (0);
