e3b65e28ebdbefdb1565a0935ee7421d0152057d
Alexander Mann <amann@student.hive.fi>
2022-11-22 18:31:19 +0200

    and_or value can now be read during execution

--- src/ast/ast_pipe_sequence.c
+++ src/ast/ast_pipe_sequence.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/19 12:55:17 by amann             #+#    #+#             */
-/*   Updated: 2022/11/22 18:11:54 by amann            ###   ########.fr       */
+/*   Updated: 2022/11/22 18:29:54 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,6 +62,8 @@ bool	ast_pipe_sequence(t_token **cursor, t_ast **node)
 	if (!*cursor || read_token(cursor, TOKEN_SEMICOLON, reset)
 		|| (read_token(cursor, TOKEN_AMPERSAND | TOKEN_PIPE, reset) && (ft_strlen(reset->value) == 2)))
 	{
+		if (read_token(cursor, TOKEN_AMPERSAND | TOKEN_PIPE, reset))
+			(*node)->and_or = (*cursor)->type;
 		eat_token(cursor, TOKEN_SEMICOLON | TOKEN_AMPERSAND | TOKEN_PIPE, reset);
 		return (true);
 	}
@@ -71,6 +73,8 @@ bool	ast_pipe_sequence(t_token **cursor, t_ast **node)
 			|| ((*cursor)->type == TOKEN_PIPE && ft_strlen((*cursor)->value) == 2)
 			|| ((*cursor)->type == TOKEN_AMPERSAND && ft_strlen((*cursor)->value) == 2)))
 	{
+		if ((*cursor)->type == TOKEN_PIPE || (*cursor)->type == TOKEN_AMPERSAND)
+			(*node)->and_or = (*cursor)->type;
 		*cursor = (*cursor)->next;
 	}
 	return (true);
