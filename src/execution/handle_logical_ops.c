7a1e44c3901d4c23f8a9cfaa84bba017bfadd263
Alexander Mann <amann@student.hive.fi>
2022-11-23 18:02:07 +0200

    removing commented out debug lines

--- src/execution/handle_logical_ops.c
+++ src/execution/handle_logical_ops.c
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/23 15:58:45 by amann             #+#    #+#             */
-/*   Updated: 2022/11/23 16:56:22 by amann            ###   ########.fr       */
+/*   Updated: 2022/11/23 18:01:16 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,6 @@
 
 void	handle_logical_ops(t_ast **ast, t_state *s, int *i)
 {
-//	t_ast *node
-
-//	node = ast[*i];
-//	while (node->left && node->left->node_type == AST_PIPE_SEQUENCE)
-//		node = node->left;
-
-//	ft_printf("%d %d\n", s->last_return_value, ast[*i]->and_or);
 	if ((ast[*i]->and_or == TOKEN_AMPERSAND && s->last_return_value == 0)
 		|| (ast[*i]->and_or == TOKEN_PIPE && s->last_return_value != 0))
 		(*i)++;
@@ -33,7 +26,6 @@ void	handle_logical_ops(t_ast **ast, t_state *s, int *i)
 	}
 	else if (ast[*i]->and_or == TOKEN_PIPE && s->last_return_value == 0)
 	{
-//		ft_putendl("here");
 		while (ast[*i] && ast[*i]->and_or == TOKEN_PIPE)
 			(*i)++;
 		if (ast[*i])
