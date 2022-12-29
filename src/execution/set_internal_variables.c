da3c9a8b80db93dddeddca6b7ba95a470d46d971
Alexander Mann <amann@student.hive.fi>
2022-12-29 12:00:07 +0200

    added checks to set_internal_variables so any reassignments of existing variables in the env or exported list are updated as well

--- src/execution/set_internal_variables.c
+++ src/execution/set_internal_variables.c
@@ -6,7 +6,7 @@
 /*   By: amann <amann@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 13:29:32 by amann             #+#    #+#             */
-/*   Updated: 2022/12/22 13:45:46 by amann            ###   ########.fr       */
+/*   Updated: 2022/12/29 11:49:33 by amann            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,10 +19,6 @@ bool	set_internal_variables(char **var_list, t_state *state)
 	char	*name;
 	char	*value;
 
-	if (!(state->intern))
-		state->intern = (char **) ft_memalloc(sizeof(char *) * (INPUT_MAX_SIZE / 2));
-	if (!(state->intern))
-		return (print_error(false, ERRTEMPLATE_SIMPLE, ERR_MALLOC_FAIL));
 	i = 0;
 	while (var_list[i])
 	{
@@ -33,7 +29,18 @@ bool	set_internal_variables(char **var_list, t_state *state)
 		value = ft_strchr(var_list[i], '=');
 		value += 1;
 		if (!env_set(name, value, &(state->intern)))
-			return (false);
+				return (false);
+
+		if (env_get(name, state->exported))
+		{
+			if (!env_set(name, value, &(state->exported)))
+				return (false);
+		}
+		if (env_get(name, state->env))
+		{
+			if (!env_set(name, value, &(state->env)))
+				return (false);
+		}
 		ft_strdel(&name);
 		i++;
 	}
