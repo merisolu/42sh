faa447827ebfd1514c2d9b73dfe5deccc3151254
Juha Manner <jumanner@student.hive.fi>
2023-01-23 15:05:32 +0200

    Moved process_group_set() to process_group.c.

---
+++ src/execution/process_group.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   process_group.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/23 15:02:30 by jumanner          #+#    #+#             */
+/*   Updated: 2023/01/23 15:02:43 by jumanner         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "execution.h"
+
+/*
+ * Moves the pid to the first pid of the job, or if there's no valid first pid
+ * creates a new process group. If 'foreground' is set to true, the process
+ * group will be set as the foreground process group.
+ */
+pid_t	process_group_set(pid_t pid, pid_t job_first_pid, bool foreground)
+{
+	pid_t	target;
+
+	if (job_first_pid > 0)
+		target = job_first_pid;
+	else
+		target = pid;
+	if (setpgid(pid, target) == -1)
+		return (-1);
+	if (foreground)
+	{
+		if (ioctl(STDIN_FILENO, TIOCSPGRP, &target) == -1)
+			return (-1);
+	}
+	return (target);
+}
