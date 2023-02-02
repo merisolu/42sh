8e1fa870cbe92d2caa159c6cc14327a52c8146a8
Juha Manner <jumanner@student.hive.fi>
2023-02-02 12:47:05 +0200

    Added support for expanding multiple history expansions at once.

--- src/history/history_expansions.c
+++ src/history/history_expansions.c
@@ -6,11 +6,39 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/01 14:10:58 by jumanner          #+#    #+#             */
-/*   Updated: 2023/02/01 14:29:08 by jumanner         ###   ########.fr       */
+/*   Updated: 2023/02/02 12:35:32 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "history.h"
+
+static char	*parse_expansion(char *expansion)
+{
+	size_t	length;
+
+	length = 0;
+	while (!ft_is_whitespace(expansion[length]) && expansion[length])
+		length++;
+	return (ft_strndup(expansion, length));
+}
+
+static char	*find_expansion(char *input)
+{
+	char	*cursor;
+
+	cursor = input;
+	while (*cursor)
+	{
+		if (*cursor != '!')
+		{
+			cursor++;
+			continue ;
+		}
+		if (*cursor != ' ' && *cursor != '\t' && *cursor != '\n')
+			return (cursor);
+	}
+	return (NULL);
+}
 
 /* 
  * Applies history expansions to the input context in *state. Returns 0 if no
@@ -19,13 +47,28 @@
 int	history_expand(t_state *state)
 {
 	char	*temp;
+	char	*target;
+	char	*expansion;
 
-	if (!ft_strstr(state->input_context.input, "!!"))
+	expansion = find_expansion(state->input_context.input);
+	if (!expansion)
 		return (0);
-	temp = ft_strreplace(state->input_context.input, "!!", state->history[1]);
-	if (!temp)
-		return (-1);
-	ft_strncpy(state->input_context.input, temp, INPUT_MAX_SIZE);
-	free(temp);
+	while (expansion)
+	{
+		target = parse_expansion(expansion);
+		if (!target)
+			return (-1);
+		temp = ft_strreplace(state->input_context.input, target,
+				state->history[1]);
+		if (!temp)
+		{
+			free(target);
+			return (-1);
+		}
+		ft_strncpy(state->input_context.input, temp, INPUT_MAX_SIZE);
+		expansion = find_expansion(expansion + ft_strlen(target) + 1);
+		free(temp);
+		free(target);
+	}
 	return (1);
 }
