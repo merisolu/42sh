d525c88072cbb3d03e3cd859ffc5fbd96ea70b2e
Juha Manner <jumanner@student.hive.fi>
2023-02-13 11:32:59 +0200

    Fixed get_job() not printing an error when it found a just-created job.

--- src/built-ins/fg_bg_utils.c
+++ src/built-ins/fg_bg_utils.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/24 14:21:51 by jumanner          #+#    #+#             */
-/*   Updated: 2023/02/13 10:21:16 by jumanner         ###   ########.fr       */
+/*   Updated: 2023/02/13 11:31:48 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,9 @@ t_job	*get_job(char *const *args, t_state *state)
 			job_id_to_job(args[1], state, &job), args[0], args[1]);
 		if (job && job->state == JOB_CREATED)
 			job = NULL;
+		else if (job)
+			print_error(1, ERRTEMPLATE_DOUBLE_NAMED,
+				args[0], args[1], ERR_NO_SUCH_JOB);
 		return (job);
 	}
 	if (!state->current_job)
