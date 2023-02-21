a8af7916ac6be0a47c0fec5ebd7651cd8093f63c
Juha Manner <jumanner@student.hive.fi>
2023-02-21 14:13:55 +0200

    Fc built-in now accepts multiple replacements.

--- src/built-ins/cmd_fc.c
+++ src/built-ins/cmd_fc.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 13:03:39 by jumanner          #+#    #+#             */
-/*   Updated: 2023/02/21 12:51:04 by jumanner         ###   ########.fr       */
+/*   Updated: 2023/02/21 14:00:31 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,8 +40,8 @@ static int	edit_and_execute(char *const *args, char *flags, t_fc_range *range,
 	return (cmd_fc_read_and_execute_file(state));
 }
 
-static int	execute_from_history(t_fc_range *range, char *replace,
-								t_state *state)
+static int	execute_from_history(t_fc_range *range,
+								char *replace[FC_MAX_REPLACE], t_state *state)
 {
 	int		i;
 	char	*temp;
@@ -50,7 +50,7 @@ static int	execute_from_history(t_fc_range *range, char *replace,
 	while (1)
 	{
 		clear_input(&(state->input_context));
-		temp = cmd_fc_apply_replacement(
+		temp = cmd_fc_apply_replacements(
 				state->history[history_number_to_index(i, state)],
 				replace);
 		if (!temp)
@@ -95,14 +95,16 @@ int	cmd_fc(char *const *args, t_state *state)
 	char		flags[6];
 	t_fc_range	range;
 	int			offset;
-	char		*replace;
+	char		*replace[FC_MAX_REPLACE];
 
+	ft_bzero(replace, sizeof(char *) * FC_MAX_REPLACE);
 	offset = cmd_fc_parse_flags(args + 1, "elnrs", flags, &on_error);
 	if (offset == -1)
 		return (1);
-	replace = cmd_fc_parse_replacement(*(args + offset + 1));
-	if (!cmd_fc_parse_range(args + offset + (replace != NULL) + 1, flags,
-			&range, state))
+	if (ft_strchr(flags, 's'))
+		cmd_fc_parse_replacements(args + offset + 1, replace);
+	if (!cmd_fc_parse_range(args + offset + \
+		ft_null_array_len((void **)replace) + 1, flags, &range, state))
 		return (1);
 	if (ft_strchr(flags, 'r'))
 		cmd_fc_reverse_range(&range);
