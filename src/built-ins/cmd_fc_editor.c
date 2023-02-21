e59b6a33b9f79c544ca8b4f04f9cc385b668afd6
Juha Manner <jumanner@student.hive.fi>
2023-02-21 14:45:17 +0200

    start_editor() now uses ERR_NO_COMMAND_FOUND define for error print.

--- src/built-ins/cmd_fc_editor.c
+++ src/built-ins/cmd_fc_editor.c
@@ -6,7 +6,7 @@
 /*   By: jumanner <jumanner@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/16 13:23:36 by jumanner          #+#    #+#             */
-/*   Updated: 2023/01/27 15:01:52 by jumanner         ###   ########.fr       */
+/*   Updated: 2023/02/21 14:38:48 by jumanner         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,7 +24,7 @@ static bool	start_editor(char *editor, t_state *state)
 	find_binary(editor, state, &path, true);
 	if (path == NULL)
 		return (print_error_bool(
-				false, ERRTEMPLATE_NAMED, "fc", "no command found"));
+				false, ERRTEMPLATE_NAMED, "fc", ERR_NO_COMMAND_FOUND));
 	fork_result = fork();
 	if (fork_result == -1)
 		return (print_error_bool(
