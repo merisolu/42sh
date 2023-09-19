8cf8018f13815b68e26888cb43b8f0f9e82bd01c
AlexMannDesigns <alex.mann.designs@gmail.com>
2023-09-19 22:23:29 +0300

    removing some unnecessary args from run, removing string interpolation in function calls as this is no longer needed. Added some failing and as yet unimplemented tests to a separate file

--- test/ShellLibrary.py
+++ test/ShellLibrary.py
@@ -3,30 +3,35 @@ from subprocess import run, PIPE, Popen
 
 # timeout in seconds for tests
 TIMEOUT = 5
+ECHO = "echo"
 
 def run_command(arg_string: str, shell_path: str) -> dict:
     """
         Runs the given "arg_string" in the shell identified by "shell_path"
+
+        Popen executes a child process. For our purposes, we are echo-ing the test case
+        specified in arg_string onto a pipe, which can then be read by the shell.
+
         run function: https://docs.python.org/3/library/subprocess.html#subprocess.run
-        The command-line ran simply echoes the arg_string onto a pipe, which is then
-        read from and executed by the shell binary.
         Run returns a class instance from which we can access the necessary outputs and
         return values for the purposes of testing.
     """
-    ps = Popen(('echo', f'{arg_string}'), stdout=PIPE)
+    ps = Popen((ECHO, arg_string), stdout=PIPE)
+
     result = run(
-        f'{shell_path}',
-        shell=True,
+        shell_path,
         capture_output=True,
         stdin=ps.stdout,
-        text=True,
+        #text=True, # True returns a string, False returns byte-code
         timeout=TIMEOUT,
-        executable="/bin/bash"
         )
+
+    # debugging
     console(f'\ncase: {arg_string} | shell = {shell_path}')
     console(f"return = {result.returncode}")
     console(f"output = {result.stdout}")
     console(f"error = {result.stderr}")
+
     return dict(
         output=result.stdout,
         err_output=result.stderr,
